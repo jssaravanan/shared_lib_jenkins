ead6049720cc3295f80575d8a5b04c4a5aa2aafd
jssaravanan <jssaravanan@gmail.com>
2021-08-26 23:06:43 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,11 +1,11 @@
 /*def call()
 {
 	echo "welcome to Groovy"
-}*/
+}
 
 def call(String name){
 	sh "echo Welcome $name to Groovy"
-}
+}*/
 
 def call(Map config = [:]){
 	sh "echo Welcome $config.name to $config.subject"
